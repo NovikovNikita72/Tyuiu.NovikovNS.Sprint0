3c8f49179ff1bbdfc565efcd4f32a56acd7d2dcd
NovikovNikita72 <145142247+NovikovNikita72@users.noreply.github.com>
2023-09-17 23:30:37 +0500

    Task 4

---
+++ Tyuiu.NovikovNS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NovikovNS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.NovikovNS.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.ReadKey();
+        }
+    }
+}
